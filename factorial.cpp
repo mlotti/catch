24021fbffc092c3b2400f029f90396d8dbd66e00
mlotti <mikko.lotti@cern.ch>
2024-08-05 14:33:44 +0200

    added test cases

--- factorial.cpp
+++ factorial.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
+#include <catch2/catch_test_macros.hpp>
 
-unsigned int Factorial( unsigned int number ) {
+int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-int main()
-{
-
-
-  int x = Factorial(1);
-
-  std::cout << "---------------------- " << "\n";
-  std::cout << x << "\n";
-  std::cout << "---------------------- " << "\n";
-
-  return 0;
+TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
+    REQUIRE( Factorial(0) == 1 );
 }
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
